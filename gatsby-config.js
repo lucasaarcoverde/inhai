974de5604d4056d8016e3eb7a4c8284284b32e4c
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-03-29 01:30:13 -0300

    Test pwacompat

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
-        display: `standalone`,
       },
     },
     {
--- src/components/Helmet/index.tsx
+++ src/components/Helmet/index.tsx
@@ -12,7 +12,6 @@ export const PageWrapper = () => {
         class: 'html',
       }}
     >
-      <link rel="manifest" href="manifest.webmanifest" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       <meta name="apple-mobile-web-app-title" content="Inhaí" />
@@ -20,6 +19,14 @@ export const PageWrapper = () => {
         type="text/javascript"
         src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"
       ></script>
+      <link rel="manifest" href="manifest.webmanifest" />
+      <script async src="path/to/pwacompat.min.js"></script>
+      <link
+        rel="icon"
+        type="image/png"
+        href="res/icon-128.png"
+        sizes="128x128"
+      />
     </Helment>
   )
 }
