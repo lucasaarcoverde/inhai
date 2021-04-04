88054520672b47ceaf9e4f1c2f379f87297d9b23
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-04-04 18:50:04 -0300

    Fix about-us page

--- src/templates/AboutUsPage.tsx
+++ src/templates/AboutUsPage.tsx
@@ -45,11 +45,13 @@ const AboutUsPage = ({
         height="calc(100vh - 56px)"
         maxH="-webkit-fill-available"
         padding="6"
+        fontSize="sm"
+        fontWeight="medium"
       >
         {children}
         <Stack spacing="3" divider={<StackDivider />}>
           <FaqSection title="O que são locais LGBTI+ friendly?">
-            <Text fontSize="sm" fontWeight="medium">
+            <Text>
               É exatamente qualquer lugar em que as pessoas da comunidade LGBTI+
               conseguem se sentir seguras, aceitas e à vontade. Seja uma
               padaria, academia, restaurante ou barzinho, são todos locais
@@ -58,12 +60,12 @@ const AboutUsPage = ({
             </Text>
           </FaqSection>
           <FaqSection title="O que fazemos com as avaliações">
-            <Text fontSize="sm" fontWeight="medium">
+            <Text>
               A ideia desse projeto é que consígamos mapear locais LGBTI+
               friendly na cidade de forma colaborativa e tornar fácil a
               visualização desses lugares para as pessoas da comunidade.
             </Text>
-            <Text fontSize="sm" fontWeight="medium">
+            <Text>
               Também pretendemos entrar em contato com locais avaliados
               negativamente, enviando todas as avaliações
               <strong>(de forma anônima)</strong> e, quem sabe, incentivando-os
@@ -71,30 +73,29 @@ const AboutUsPage = ({
             </Text>
           </FaqSection>
           <FaqSection title="Sobre o desenvolvimento">
-            <Text fontSize="sm" fontWeight="medium">
+            <Text>
               Me chamo Lucas Arcoverde, sou de Campina Grande, na paraíba, e
               atualmente estou cursando o meu último período em Ciência da
               Computação pela Universidade Federal de Campina Grande.
             </Text>
-            <Text fontSize="sm" fontWeight="medium">
+            <Text>
               Esse projeto está sendo feito para o meu trabalho de conclusão de
               curso e tem os seguintes objetivos:
-              <OrderedList marginTop="2">
-                <ListItem>
-                  Tornar transparente para a comunidade LGBTI+ que há sim locais
-                  em que podemos nos sentir nós mesmos sem nenhum medo ou
-                  receio.
-                </ListItem>
-                <ListItem>
-                  Criar um ambiente colaborativo em que a gente possa avaliar e
-                  expor nossa opinião sobre os locais que frequentamos.
-                </ListItem>
-                <ListItem>
-                  Incentivar os locais a se preocuparem com o ambiente que estão
-                  sendo criados.
-                </ListItem>
-              </OrderedList>
             </Text>
+            <OrderedList paddingX="6">
+              <ListItem>
+                Tornar transparente para a comunidade LGBTI+ que há sim locais
+                em que podemos nos sentir nós mesmos sem nenhum medo ou receio.
+              </ListItem>
+              <ListItem>
+                Criar um ambiente colaborativo em que a gente possa avaliar e
+                expor nossa opinião sobre os locais que frequentamos.
+              </ListItem>
+              <ListItem>
+                Incentivar os locais a se preocuparem com o ambiente que estão
+                sendo criados.
+              </ListItem>
+            </OrderedList>
           </FaqSection>
         </Stack>
         <Center width="100%" paddingBottom="72px">
