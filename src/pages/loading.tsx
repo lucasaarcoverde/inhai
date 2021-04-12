e839735c0680d07b1b5fabf4b1168d5cbf1a8745
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-04-12 20:20:10 -0300

    Add login redirect

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,9 +1,38 @@
 import * as React from 'react'
-import { FirebaseProvider } from '../contexts/firebase'
+import { FirebaseProvider, useAuth } from '../contexts/firebase'
 
 import { InitialLoading } from '../components/Loading'
+import { navigate } from 'gatsby'
 
 const LoadingPage = () => {
+  const [loading, setLoading] = React.useState(true)
+
+  const { firebase } = useAuth()
+
+  React.useEffect(() => {
+    if (!loading) {
+      navigate('/login')
+    }
+  }, [loading])
+
+  React.useEffect(() => {
+    const loadingTimer = setTimeout(() => setLoading(false), 15000)
+
+    firebase.auth().onAuthStateChanged((authUser) => {
+      if (!authUser) return
+
+      const navigateTimer = setTimeout(() => navigate('/app/map'), 1000)
+
+      return () => {
+        clearTimeout(navigateTimer)
+      }
+    })
+
+    return () => {
+      clearTimeout(loadingTimer)
+    }
+  }, [firebase])
+
   return (
     <FirebaseProvider>
       <InitialLoading />
