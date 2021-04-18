53b6681160abd75f100147ebe711e9b95e22bf54
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-04-18 18:08:52 -0300

    Update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,13 +51,3 @@ module.exports = {
     },
   ],
 }
-//   plugins: [
-//     {
-//       resolve: `gatsby-plugin-ts-config`,
-//       options: {
-//         configDir: '/src/gatsby/',
-//         tsNode: true,
-//       },
-//     },
-//   ],
-// }
