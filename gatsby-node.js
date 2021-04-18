3f4a88953ba622b7a19295206ecb67eb143e91db
lucasaarcoverde <lucasaarcoverde@gmail.com>
2021-04-18 17:59:49 -0300

    Fix webpack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,14 @@
-exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === 'build-html') {
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html' || stage === 'develop-html') {
     actions.setWebpackConfig({
-      // Don't bundle modules that reference browser globals such as `window` and `IDBIndex` during SSR.
-      // See: https://github.com/gatsbyjs/gatsby/issues/17725
-      externals: getConfig().externals.concat((_context, request, callback) => {
-        // Exclude bundling firebase* and react-firebase*
-        // These are instead required at runtime.
-        if (/^@?(react-)?firebase(.*)/.test(request)) {
-          return callback(null, `umd ${request}`)
-        }
-
-        callback()
-      }),
+      module: {
+        rules: [
+          {
+            test: /^@?(react-)?firebase(.*)/,
+            use: loaders.null(),
+          },
+        ],
+      },
     })
   }
 }
